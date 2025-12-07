7076b59b42952d1b0c3a6f669a3c052f4e14490d
VVolfire <volckoffvladis@yandex.ru>
2025-12-07 19:46:40 +0300

    finished

--- src/components/SurveyForm.vue
+++ src/components/SurveyForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import ProgressBar from './card/ProgressBar.vue'
 import QuestionCard from './card/QuestionCard.vue'
 import NavigationButtons from './card/NavigationButtons.vue'
@@ -13,13 +13,30 @@ const props = defineProps({
     type: Object,
     default: () => ({}),
   },
+  currentStep: {
+    type: Number,
+    default: 0,
+  },
 })
 
-const emit = defineEmits({ answer: null })
+const emit = defineEmits({
+  answer: null,
+  'update:currentStep': null,
+  complete: null,
+})
 
-const currentStep = ref(0)
+const currentStep = ref(props.currentStep)
+
+// Синхронизируем с props
+watch(
+  () => props.currentStep,
+  (newValue) => {
+    currentStep.value = newValue
+  },
+)
+
 const currentQuestion = computed(() => props.questions[currentStep.value])
-const currentAnswerQuestion = computed(() => props.answers[currentQuestion.value.id])
+const currentAnswerQuestion = computed(() => props.answers[currentQuestion.value?.id])
 const isLastStep = computed(() => currentStep.value === props.questions.length - 1)
 
 const canGoNext = computed(() => {
@@ -35,14 +52,27 @@ const canGoNext = computed(() => {
   return !!answer
 })
 
-function onAnswer() {}
+function onAnswer(payload) {
+  emit('answer', payload)
+}
 
 function onGoNext() {
   if (!canGoNext.value) return
-  if (!isLastStep.value) currentStep.value++
+  if (!isLastStep.value) {
+    currentStep.value++
+    emit('update:currentStep', currentStep.value)
+  }
 }
+
 function onGoBack() {
-  if (currentStep.value > 0) currentStep.value--
+  if (currentStep.value > 0) {
+    currentStep.value--
+    emit('update:currentStep', currentStep.value)
+  }
+}
+
+function onComplete() {
+  emit('complete')
 }
 </script>
 
@@ -53,11 +83,15 @@ function onGoBack() {
     v-if="currentQuestion"
     :question="currentQuestion"
     :answer-question="currentAnswerQuestion"
+    @answer="onAnswer"
   />
 
   <NavigationButtons
     :can-go-back="currentStep > 0"
     :can-go-next="canGoNext"
     :is-last-step="isLastStep"
+    @next="onGoNext"
+    @back="onGoBack"
+    @complete="onComplete"
   />
 </template>
